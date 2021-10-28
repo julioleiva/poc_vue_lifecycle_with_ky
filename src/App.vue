fb5d636998ede0ce3631ae45dd61e3413e27ab12
Julio Leiva <leivadiazjulio@gmail.com>
2021-10-28 12:33:23 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="app">
+    <h2>POC KY.js y Ciclos de vida</h2>
+    <div>
+      <a href="https://es.vuejs.org/v2/guide/instance.html">Instancia VUE</a>
+    </div>
+  </div>
+</template>
+
+<script>
+import ky from "ky";
+
+async function fetchData() {
+  let response = await ky("https://jsonplaceholder.typicode.com/posts");
+  let body = await response.json();
+  return body;
+}
+
+export default {
+  name: "App",
+  beforeCreate() {
+    // Se inician los eventos y el ciclo de vida
+    // Aún no hay acceso a la data
+    console.log("beforeCreate, sin acceso a la data. Data:", this.posts);
+  },
+  created() {
+    // Se inician las injections y la reactividad
+    // Ya hay acceso a la data
+    console.log("created, con acceso a la data. Data:", this.posts);
+    console.log(this.posts);
+    fetchData();
+  },
+  beforeMount() {},
+  mounted() {},
+  beforeUpdate() {},
+  update() {},
+  beforeDestroy() {},
+  destroyed() {},
+
+  data() {
+    return {
+      posts: [],
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
